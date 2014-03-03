fa10aa13812c006e02669e4394fee68ffd2be609
jxnblk <jackson@kickstarter.com>
2014-03-03 10:52:07 -0500

    Better injection and audio icons

--- geomicons.js
+++ geomicons.js
@@ -1,81 +1,33 @@
-// Geomicons image replacement js
-//
-// This currently creates a second network request
-// for each icon.
-// To do: get icon from cache, or prevent downloads
-// of img src
+// Geomicons SVG injection
 
 var icons = document.getElementsByClassName('geomicon'),
-    spriteIcons = document.getElementsByClassName('geo-sprite'),
+    iconUrl = 'go-sprite.svg',
+    xmlHttp = null,
     injectSvg,
-    getIcon;
+    sprite;
+
+xmlHttp = new XMLHttpRequest();
+xmlHttp.open('GET', iconUrl, false);
+xmlHttp.send(null);
+
+if(xmlHttp.responseXML) {
+  sprite = xmlHttp.responseXML.documentElement;
+} else {
+  console.error('Geomicon sprite not found - check iconUrl variable.');
+}
 
 injectSvg = function(el, svg){
   svg.setAttribute('class', el.className);
   el.parentNode.replaceChild(svg, el);
 };
 
-//getIcon = function(el, path) {
-//  xhr(path, function(err, resp) {
-//    if(resp) {
-//      var svg = resp.responseXML.documentElement;
-//      injectSvg(el, svg);
-//    }
-//    if(err) console.log(err);
-//  }, true);
-//};
-
-//for (var i = 0; i < icons.length; i++) {
-//  var icon = icons[i];
-//  var path = icon.getAttribute('src');
-//  if (!path) path = icon.getAttribute('data-src');
-//  getIcon(icon, path);
-//};
-
-
-// Sprite method
-
-var sprite;
-
-function httpGet(url) {
-  var xmlHttp = null;
-  xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", url, false );
-  xmlHttp.send( null );
-  return xmlHttp;
-}
-
-var sprite = httpGet('go-sprite.svg').responseXML.documentElement;
-
-/*
-  function getSprite() {
-
-    console.log(sprite);
-    // Probably don't need this for same-origin requests
-    //xhr('go-sprite.svg', function(err, resp) {
-    //  if(resp) {
-    //    var sprite = resp.responseXML.documentElement;
-    //    console.log(sprite);
-    //    parseSpriteIcons(sprite);
-    //  }
-    //  if(err) console.log(err);
-    //}, true);
-  };
-*/
-
-//getSprite();
-
-//function parseSpriteIcons(sprite) {
-  for (var i = 0; i < spriteIcons.length; i++){
-    var icon = spriteIcons[i],
-        id = icon.getAttribute('data-id'),
-        svg = sprite.getElementById(id).cloneNode(true);
-    svg.removeAttribute('transform');
-    svg.id = id + '-' + i;
-
-    icon.classList.add('geomicon-' + id);
-    console.log(svg);
-    injectSvg(icon, svg);
-  };
-//};
+for (var i = 0; i < icons.length; i++){
+  if (!sprite) return false;
+  var icon = icons[i],
+      id = icon.getAttribute('data-id'),
+      svg = sprite.getElementById(id).cloneNode(true);
+  svg.id = id + '-' + i;
+  icon.className += ' geomicon-' + id;
+  injectSvg(icon, svg);
+};
 
