de3198b8924fdc47ce189d9ddf0f37db427898a4
Brent Jackson <jxnblk@gmail.com>
2014-03-03 22:05:15 -0500

    Disabling svgmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var svgmin = require('gulp-svgmin');
+//var svgmin = require('gulp-svgmin');
 var concat = require('gulp-concat');
 
 gulp.task('default', function() {
@@ -9,8 +9,7 @@ gulp.task('default', function() {
 gulp.task('sprite', function() {
   var stream = gulp.src(['./partials/_sprite-head.svg', './icons/*.svg', './partials/_sprite-footer.svg'])
     .pipe(concat('sprite.svg'))
-    //.pipe(svgmin([ { cleanupIDs: { params: { remove: false } } } ]))
-    .pipe(svgmin())
+    //.pipe(svgmin())
     .pipe(gulp.dest('./sprite'));
 });
 
