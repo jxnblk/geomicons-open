2a9a9437aad12a421a4dfcd3981bee51582f830a
jxnblk <jackson@kickstarter.com>
2014-03-04 12:27:23 -0500

    Add defs header and footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var svgmin = require('gulp-svgmin');
 var concat = require('gulp-concat');
+var header = require('gulp-header');
+var footer = require('gulp-footer');
 var cheerio = require('gulp-cheerio');
 
 gulp.task('default', function() {
@@ -10,12 +12,11 @@ gulp.task('default', function() {
 gulp.task('sprite', function() {
   var stream = gulp.src(['./partials/_sprite-head.svg', './icons/*.svg', './partials/_sprite-footer.svg'])
     .pipe(concat('sprite.svg'))
-    .pipe(svgmin([{ cleanupIDs: false }]))
+    //.pipe(svgmin([{ cleanupIDs: false }]))
     .pipe(gulp.dest('./sprite'));
 });
 
 gulp.task('defs', function() {
-  //var stream = gulp.src(['./partials/_defs-head.svg', './icons/*.svg', './partials/_defs-footer.svg'])
   var stream = gulp.src('./icons/*.svg')
     .pipe(cheerio({
       run: function ($) {
@@ -26,6 +27,10 @@ gulp.task('defs', function() {
         }
     }))
     .pipe(concat('defs.svg'))
+    .pipe(header('<svg xmlns="http://www.w3.org/2000/svg"><defs>'))
+    .pipe(footer('</defs></svg>'))
+    // svgmin removes ids - need to disable this
+    //.pipe(svgmin())
     .pipe(gulp.dest('./sprite'));
 });
 
