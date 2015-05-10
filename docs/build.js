1bfe5ba4dbe30d832a1adec56f2b29a1289b6fdf
jxnblk <jacksonblack@gmail.com>
2015-05-10 11:33:33 -0400

    Inline css

---
+++ docs/build.js
@@ -0,0 +1,24 @@
+
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+var cssnext = require('cssnext');
+var data = require('../package.json');
+
+var tpl = _.template(fs.readFileSync(path.join(__dirname, 'template.html'), 'utf8'));
+var html;
+
+data.css = cssnext('@import "blk"; @import "./docs/geomicons";', {
+  compress: true,
+  features: {
+    colorRgba: false,
+    rem: false,
+    pseudoElements: false,
+  }
+});
+
+
+html = tpl(data);
+
+fs.writeFileSync(path.join(__dirname, '../index.html'), html);
+
